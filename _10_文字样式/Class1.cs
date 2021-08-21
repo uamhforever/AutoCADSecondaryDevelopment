6d402d0fe07f40a896fb3d33662fab5395600d0b
Qu Yong <570744756@qq.com>
2021-08-21 16:34:36 +0800

    _10_文字样式

---
+++ _10_文字样式/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace _10_文字样式
+{
+    public class Class1
+    {
+        [CommandMethod("TestStyleDemo")]
+        public void TextStyleDemo()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            db.AddTextStyle("测试文字样式");
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                TextStyleTable tst = (TextStyleTable)trans.GetObject(db.TextStyleTableId, OpenMode.ForRead);
+                foreach (var item in tst)
+                {
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ _10_文字样式/TextStyleTools.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_文字样式
+{
+    public static partial class TextStyleTools
+    {
+        /// <summary>
+        /// 添加文字样式
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="textStyleName"><文字样式名称/param>
+        public static void AddTextStyle(this Database db, string textStyleName)
+        {
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                // 打开文字样式表
+                TextStyleTable tst = (TextStyleTable)trans.GetObject(db.TextStyleTableId, OpenMode.ForRead);
+                if(!tst.Has(textStyleName))
+                {
+                    // 声明文字样式表记录
+                    TextStyleTableRecord tstr = new TextStyleTableRecord();
+                    tstr.Name = textStyleName;
+                    // 把新的文字样式表记录加入文字样式表
+                    tst.UpgradeOpen();
+                    tst.Add(tstr);
+                    //更新
+                    trans.AddNewlyCreatedDBObject(tstr, true);
+                    tst.DowngradeOpen();
+                    // 提交事务处理
+                    trans.Commit();
+                }
+            }
+        }
+    }
+}
