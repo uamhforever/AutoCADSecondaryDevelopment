1f4db5961d72bd6b22a91ce5a9f62fe5421eec2e
Qu Yong <570744756@qq.com>
2021-08-21 15:38:26 +0800

    _06_Text

---
+++ _06_Text/Class1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoCADDotNetTools;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace _06_Text
+{
+    public class Class1
+    {
+        [CommandMethod("TextDemo")]
+        public void TextDemo()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+
+            // 绘制一些沿Y轴增长的直线
+            Line[] line = new Line[10];
+            Point3d[] pt1 = new Point3d[10];
+            Point3d[] pt2 = new Point3d[10];
+            for (int i = 0; i < line.Length; i++)
+            {
+                pt1[i] = new Point3d(50, 50 + 20 * i, 0);
+                pt2[i] = new Point3d(150, 50 + 20 * i, 0);
+                line[i] = new Line(pt1[i], pt2[i]);
+            }
+            db.AddEntityToModeSpace(line);
+
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                DBText text0 = new DBText(); // 新建单行文本对象
+                text0.Position = pt1[0]; // 设置文本位置 
+                text0.TextString = "你好 数据智能笔记！text0"; // 设置文本内容
+                text0.Height = 5;  // 设置文本高度
+                text0.Rotation = Math.PI * 0.5;  // 设置文本选择角度
+                text0.IsMirroredInX = true; // 在X轴镜像
+                text0.HorizontalMode = TextHorizontalMode.TextCenter; // 设置对齐方式
+                text0.AlignmentPoint = text0.Position; //设置对齐点
+                db.AddEntityToModeSpace(text0);
+
+                DBText text1 = new DBText // 新建单行文本对象
+                {
+                    Position = pt1[1],
+                    TextString = "你好 数据智能笔记！text1",
+                    Height = 10,
+                    Rotation = Math.PI * 0.1,
+                    IsMirroredInY = true, // 在Y轴镜像
+                    HorizontalMode = TextHorizontalMode.TextCenter
+                }; 
+                
+                text1.AlignmentPoint = text1.Position; ; // 设置对齐点
+                db.AddEntityToModeSpace(text1);
+
+                trans.Commit();
+            }
+
+
+
+
+        }
+
+
+        [CommandMethod("TextDemo2")]
+        public void TextDemo2()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            DBText text = new DBText();
+            text.Position = new Point3d(100, 100, 0);
+            //text.TextString = "面积≈%%U100%%U㎡";
+            text.TextString = "数据智能笔记" + TextTools.TextSpecialSymbol.Alpha + TextTools.TextSpecialSymbol.Tolerance + "0.006"
+                + TextTools.TextSpecialSymbol.Underline + "C# CAD二次开发" + TextTools.TextSpecialSymbol.Underline;
+            text.Height = 10;
+            db.AddEntityToModeSpace(text);
+        }
+
+
+        [CommandMethod("MTextDemo")]
+        public void MTextDemo()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+
+            MText mtext = new MText();  // 声明多行文本对象
+            mtext.Location = new Point3d(100, 100, 0); // 设置位置
+            mtext.Contents = "智能数据笔记 CAD二次开发系列笔记"; // 设置文本内容
+            mtext.TextHeight = 10; //设置文本高度 
+            mtext.Width = 3; // 文本框宽度
+            mtext.Height = 5; // 文本框高度
+
+            MText mtext2 = new MText();
+            mtext.Location = new Point3d(100, 100, 0);
+            mtext.Contents = "222智能数据笔记 \nCAD二次开发系列笔记";
+            mtext.TextHeight = 20;
+
+            db.AddEntityToModeSpace(mtext, mtext2);
+
+        }
+
+
+        [CommandMethod("MTextDemo2")]
+        public void MTextDemo2()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+
+            MText mtext = new MText();
+            //mtext.Contents = "\\A1;Φ20{\\H0.5x;\\SH7/P7;}";
+
+            //mtext.Contents = string.Format("\\A1;{0}{1}\\H{2}x;\\S{3}{4}{5};{6}","Φ20","{",0.2,"数据智能笔记","#","YZK","}");
+
+
+            mtext.Contents = TextTools.StackMtext(TextTools.TextSpecialSymbol.Diameter, 0.4, "+0.5", TextTools.MTextStackType.Tolerance, "-0.1");
+            mtext.Location = new Point3d(100, 100, 0);
+            mtext.TextHeight = 10;
+            db.AddEntityToModeSpace(mtext);
+        }
+
+
+        [CommandMethod("PickMTextDemo")]
+        public void PickMTextDemo()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            PromptEntityResult per = ed.GetEntity("\n 请选择多行文字");
+            if (per.Status != PromptStatus.OK) return;  // 没有选择就直接返回
+
+            Entity ent;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                ent = (Entity)per.ObjectId.GetObject(OpenMode.ForRead);
+            }
+            MText mtext = (MText)ent;
+        }
+    }
+}
---
+++ _06_Text/TextTools.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Text
+{
+    /// <summary>
+    /// 特殊字符
+    /// </summary>
+     public static partial class TextTools
+    {
+       public struct TextSpecialSymbol
+        {
+            // 符号16进制编码
+            public static readonly string Degree = @"\U+00B0"; // 角度(°)
+            public static readonly string Tolerance = @"\U+00B1"; // 公差(±）
+            public static readonly string Diameter = @"\U+00D8";  // 直径(∅)
+            public static readonly string Angle = @"\U+2220"; // 角度（∠）
+            public static readonly string AlmostEqual = @"\U+2248"; // 约等于(≈）|
+            public static readonly string LineBoundary = @"\U+E100"; // 边界线
+            public static readonly string LineCenter = @"\U+2104"; // 中心线
+            public static readonly string Delta = @"\U+0349"; // 增量(△)
+            public static readonly string ElecttricalPhase = @"\U+0278"; //电相位（φ)
+            public static readonly string LineFlow = @"\U+E101";  // 流线
+            public static readonly string Identity = @"\U+2261";  // 标识
+            public static readonly string InitialLength = @"\U+E200";// 初始长度
+            public static readonly string LineMonument = @"\U+E102"; // 界碑线
+            public static readonly string Notequal = @"\U+2260";  // 不等于（≠）
+            public static readonly string Ohm = @"\U+2126";  // 欧姆
+            public static readonly string Omega = @"\U+03A9"; // 欧米伽(Ω)
+            public static readonly string LinePlate = @"\U+0214A"; // 地界线
+            public static readonly string Subscript2 = @"\U+2082"; // 下标2
+            public static readonly string Square = @"\U+00B2"; // 平方
+            public static readonly string Cube = @"\U+00B3"; // 立方
+            public static readonly string Overline = @"%%o"; // 单行文字上划线
+            public static readonly string Underline = @"%%u";  // 单行文字下划线
+            public static readonly string Alpha = @"\U+03B1";  // 希腊字母（α）
+            public static readonly string Belta = @"\U+03B2";  // 希腊字母（β）
+            public static readonly string Gamma = @"\U+03B3";  // 希腊字母（γ）
+            public static readonly string Theta = @"\U+03B8";  // 希腊字母（θ）
+            public static readonly string SeteelBar1 = @"\U+0082";  // 一级钢筋符号
+            public static readonly string SeteelBar2 = @"\U+0083";  // 二级钢筋符号
+            public static readonly string SeteelBar3 = @"\U+0084";  // 三级钢筋符号
+            public static readonly string SeteelBa41 = @"\U+0085";  // 四级钢筋符号            
+        }
+
+        /// <summary>
+        /// 堆叠符号
+        /// </summary>
+        public struct MTextStackType
+        {
+            public static readonly string Horizental = "/"; // 水平堆叠
+            public static readonly string Italic = "=";     // 斜分堆叠
+            public static readonly string Tolerance = "^";  // 容差堆叠
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="scaleFactor"></param>
+        /// <param name="topText"></param>
+        /// <param name="stackType"></param>
+        /// <param name="bottomText"></param>
+        /// <returns></returns>
+        public static string StackMtext(string text, double scaleFactor, string topText, string stackType,string bottomText)
+        {
+            return string.Format("\\A1;{0}{1}\\H{2}x;\\S{3}{4}{5};{6}", text, "{", scaleFactor, topText, stackType, bottomText, "}");
+        }
+    }
+
+
+   
+}
